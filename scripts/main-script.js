93ce7f66511b32685f89091110c721d5d6181651
abdul <ali05abdulrahman@gmail.com>
2024-08-26 01:47:21 +0100

    added dropdown and made interactive

---
+++ scripts/main-script.js
@@ -0,0 +1,13 @@
+let isClicked = false; 
+
+const moreButton = document.querySelector('.h-div');
+const dropdown = document.querySelector('.dropdown')
+moreButton.addEventListener('click',() => {
+  if (!isClicked) {
+    dropdown.classList.remove('js-dropdown');
+    isClicked = true;
+  } else {
+    dropdown.classList.add('js-dropdown');
+    isClicked = false
+  }
+})
