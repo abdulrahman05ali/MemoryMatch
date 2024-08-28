364c78d01fb5acff5d2c8e7ba5c0cf0d737c805e
abdul <ali05abdulrahman@gmail.com>
2024-08-28 18:49:44 +0100

    playButton functional

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -11,3 +11,20 @@ moreButton.addEventListener('click',() => {
     isClicked = false;
   }
 })
+
+const playButton = document.querySelector('.playButton');
+const content = document.querySelector('.gameplay-contents');
+playButton.addEventListener('click',() => {
+  content.classList.add('hidden');
+
+  setTimeout(() => {
+    content.innerHTML = 
+    ` <div class="visuals-2">
+        <div class="image-1">
+          <img src="images/Instructions.png">
+        </div>
+    </div> `;
+
+    content.classList.remove('hidden');
+  },500)
+}) 
