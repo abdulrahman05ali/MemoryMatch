663af50a6e360ab3575539434aab2fc29c60715e
abdul <ali05abdulrahman@gmail.com>
2024-08-30 00:31:48 +0100

    back button functionality

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -12,23 +12,42 @@ moreButton.addEventListener('click',() => {
   }
 })
 
+function setOriginalPlay() {
 const playButton = document.querySelector('.playButton');
 const content = document.querySelector('.gameplay-contents');
-playButton.addEventListener('click',() => {
-  content.classList.add('hidden');
+  playButton.addEventListener('click',() => {
+    content.classList.add('hidden');
+  
+    setTimeout(() => {
+      content.innerHTML = 
+      `<div class="visuals-2">
+        <div class="image-1">
+          <img src="https://fontmeme.com/permalink/240829/7ffe2442ba9c7f5068c27c90678296c5.png">
+        </div>
+        <div class="control-buttons">
+            <img class="back" src="images/backButton.png">
+            <img class="newplay" src="images/playbutton.png">
+        </div>
+      </div> `;
+  
+      content.classList.remove('hidden');
+  
+      const backButton = document.querySelector('.back');
+      backButton.addEventListener('click',() => {
+        content.classList.add('hidden');
+  
+        setTimeout(() => {
+          content.innerHTML = 
+          `<div class="visuals-1">
+            <img src="images/play.png" class="playButton"> 
+         </div>`;
+         
+         content.classList.remove('hidden')
+         setOriginalPlay();
+        },400)
+      })
+    },400)
+  }) 
+}
 
-  setTimeout(() => {
-    content.innerHTML = 
-    `<div class="visuals-2">
-      <div class="image-1">
-        <img src="https://fontmeme.com/permalink/240829/7ffe2442ba9c7f5068c27c90678296c5.png">
-      </div>
-      <div class="control-buttons">
-          <img src="images/backButton.png">
-          <img src="images/playbutton.png">
-      </div>
-    </div> `;
-
-    content.classList.remove('hidden');
-  },400)
-}) 
+setOriginalPlay();
